7205c113956b54785f724f2e818caba837ef1cbc
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2024-09-23 23:41:31 +0200

    Generate the random code

--- mastermind.c
+++ mastermind.c
@@ -3,10 +3,20 @@
 #include <time.h>
 
 void run_game() {
-    printf("This is mastermind Simplified\n");
     int digits[4];
+    srand(time(0));
+    
+    for (int i = 0; i <4; i++) {
+        int randomNumber = (rand() % 8) + 1;
+        digits[i] = randomNumber;
+    }
+
+    printf("The code has been generated\n");
+
+    char userCode[4];
 }
 
-main() {
+int main() {
     run_game();
+    return 0;
 }
