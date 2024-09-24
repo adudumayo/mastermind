f5cb130fc197974d8009d1aadab448a79a52c264
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2024-09-24 16:57:53 +0200

    Too many changes

--- mastermind.c
+++ mastermind.c
@@ -3,27 +3,29 @@
 #include <time.h>
 
 void run_game() {
-    int digits[4];
+    int masterCode[4];
     srand(time(0));
+    char userCode[4];
     
     for (int i = 0; i <4; i++) {
-        int randomNumber = (rand() % 8) + 1;
-        digits[i] = randomNumber;
+        //int randomNumber = (rand() % 8) + 1;
+        // masterCode[i] = randomNumber;
+        masterCode[i] = i;
     }
 
     printf("The code has been generated\n");
 
-    char userCode[4];
+    // int arraySize = sizeof(masterCode)/sizeof(masterCode[0]);
 
     printf("Enter your code: ");
     scanf("%s", &userCode);
 
     for (int i = 0; userCode[i] != '\0'; i++) {
-        char digit = userCode[i];
-        if (digit == digits[i]) {
-            printf("Yeah Buddy\n");
+        char userDigit = userCode[i];
+        if (userDigit == masterCode[i]) {
+            printf("%c looks right, but is it?\n", userDigit);
         } else {
-            printf("Not quite!\n");
+            printf("%c is not right\n", userDigit);
         }
     }
 
