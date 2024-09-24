4f082e57f9c759b74e78eb43c87edff58b8cf7a5
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2024-09-24 17:10:47 +0200

    Started type casting

--- mastermind.c
+++ mastermind.c
@@ -4,8 +4,9 @@
 
 void run_game() {
     int masterCode[4];
-    srand(time(0));
     char userCode[4];
+    int userDigits[4];
+    srand(time(0));
     
     for (int i = 0; i <4; i++) {
         //int randomNumber = (rand() % 8) + 1;
@@ -15,20 +16,18 @@ void run_game() {
 
     printf("The code has been generated\n");
 
-    // int arraySize = sizeof(masterCode)/sizeof(masterCode[0]);
-
     printf("Enter your code: ");
     scanf("%s", &userCode);
 
-    for (int i = 0; userCode[i] != '\0'; i++) {
-        char userDigit = userCode[i];
-        if (userDigit == masterCode[i]) {
-            printf("%c looks right, but is it?\n", userDigit);
-        } else {
-            printf("%c is not right\n", userDigit);
-        }
+    for (int i = 0; i < 4; i++) {
+        userDigits[i] = userCode[i] - '0';
     }
 
+    printf("Checing the conversion\n");
+
+    for (int i = 0; i < 4; i++) {
+        printf("The number is %d\n", userDigits[i]);
+    }
 }
 
 int main() {
