1d060a133f648bf629a536a45fbf6d558e15da97
adudumayo023 <adudumayo023@student.wethinkcode.co.za>
2024-09-26 13:51:09 +0200

    Iteration 1 assembly

--- mastermind.s
+++ mastermind.s
@@ -2,30 +2,22 @@
 	.text
 	.section	.rodata
 .LC0:
-	.string	"Found %d\n"
-.LC1:
-	.string	"Master says %d\n"
-.LC2:
-	.string	"Duplicate found"
-.LC3:
-	.string	"idx is %d\n"
-.LC4:
 	.string	"The code is "
-.LC5:
+.LC1:
 	.string	"%d"
-.LC6:
+.LC2:
 	.string	"Enter your code: "
-.LC7:
+.LC3:
 	.string	"%4s"
-.LC8:
+.LC4:
 	.string	"Correct"
-.LC9:
+.LC5:
 	.string	"Incorrect"
 	.align 8
-.LC10:
+.LC6:
 	.string	"Number of correct digits is %d\n"
 	.align 8
-.LC11:
+.LC7:
 	.string	"Number of incorrect digits is %d\n"
 	.text
 	.globl	run_game
@@ -38,15 +30,13 @@ run_game:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	subq	$48, %rsp
+	subq	$64, %rsp
 	movl	$0, -4(%rbp)
 	movl	$0, -8(%rbp)
 	movl	$0, %edi
 	call	time@PLT
 	movl	%eax, %edi
 	call	srand@PLT
-	movl	$0, -12(%rbp)
-.L5:
 	call	rand@PLT
 	movl	%eax, %edx
 	movl	%edx, %eax
@@ -57,14 +47,65 @@ run_game:
 	subl	%eax, %edx
 	movl	%edx, %eax
 	addl	$1, %eax
-	movl	%eax, -24(%rbp)
-	movl	-24(%rbp), %eax
-	movl	%eax, %esi
+	movl	%eax, masterCode.0(%rip)
+	movl	$1, -12(%rbp)
+.L9:
+	movb	$0, -13(%rbp)
+	call	rand@PLT
+	movl	%eax, %edx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	shrl	$29, %eax
+	addl	%eax, %edx
+	andl	$7, %edx
+	subl	%eax, %edx
+	movl	%edx, %eax
+	addl	$1, %eax
+	movl	%eax, -32(%rbp)
+	movl	$0, -20(%rbp)
+	jmp	.L2
+.L5:
+	movl	-20(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	leaq	masterCode.0(%rip), %rax
+	movl	(%rdx,%rax), %eax
+	cmpl	%eax, -32(%rbp)
+	jne	.L3
+	movb	$1, -13(%rbp)
+	jmp	.L4
+.L3:
+	addl	$1, -20(%rbp)
+.L2:
+	movl	-20(%rbp), %eax
+	cmpl	-12(%rbp), %eax
+	jl	.L5
+.L4:
+	cmpb	$0, -13(%rbp)
+	jne	.L17
+	movl	-12(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rcx
+	leaq	masterCode.0(%rip), %rdx
+	movl	-32(%rbp), %eax
+	movl	%eax, (%rcx,%rdx)
+	addl	$1, -12(%rbp)
+	cmpl	$4, -12(%rbp)
+	je	.L18
+	jmp	.L9
+.L17:
+	nop
+	jmp	.L9
+.L18:
+	nop
 	leaq	.LC0(%rip), %rax
 	movq	%rax, %rdi
 	movl	$0, %eax
 	call	printf@PLT
-	movl	-12(%rbp), %eax
+	movl	$0, -24(%rbp)
+	jmp	.L10
+.L11:
+	movl	-24(%rbp), %eax
 	cltq
 	leaq	0(,%rax,4), %rdx
 	leaq	masterCode.0(%rip), %rax
@@ -74,122 +115,67 @@ run_game:
 	movq	%rax, %rdi
 	movl	$0, %eax
 	call	printf@PLT
-	movl	-12(%rbp), %eax
-	cltq
-	leaq	0(,%rax,4), %rdx
-	leaq	masterCode.0(%rip), %rax
-	movl	(%rdx,%rax), %eax
-	cmpl	%eax, -24(%rbp)
-	jne	.L2
-	movl	-12(%rbp), %eax
-	cltq
-	leaq	0(,%rax,4), %rcx
-	leaq	masterCode.0(%rip), %rdx
-	movl	-24(%rbp), %eax
-	movl	%eax, (%rcx,%rdx)
+	addl	$1, -24(%rbp)
+.L10:
+	cmpl	$3, -24(%rbp)
+	jle	.L11
+	movl	$10, %edi
+	call	putchar@PLT
 	leaq	.LC2(%rip), %rax
 	movq	%rax, %rdi
 	movl	$0, %eax
 	call	printf@PLT
-	jmp	.L3
-.L2:
-	movl	-12(%rbp), %eax
-	cltq
-	leaq	0(,%rax,4), %rcx
-	leaq	masterCode.0(%rip), %rdx
-	movl	-24(%rbp), %eax
-	movl	%eax, (%rcx,%rdx)
-	addl	$1, -12(%rbp)
-	movl	-12(%rbp), %eax
-	movl	%eax, %esi
+	leaq	-36(%rbp), %rax
+	movq	%rax, %rsi
 	leaq	.LC3(%rip), %rax
 	movq	%rax, %rdi
 	movl	$0, %eax
-	call	printf@PLT
-	cmpl	$4, -12(%rbp)
-	je	.L12
-	jmp	.L5
-.L12:
-	nop
-.L3:
-	leaq	.LC4(%rip), %rax
-	movq	%rax, %rdi
-	movl	$0, %eax
-	call	printf@PLT
-	movl	$0, -16(%rbp)
-	jmp	.L6
-.L7:
-	movl	-16(%rbp), %eax
-	cltq
-	leaq	0(,%rax,4), %rdx
-	leaq	masterCode.0(%rip), %rax
-	movl	(%rdx,%rax), %eax
-	movl	%eax, %esi
-	leaq	.LC5(%rip), %rax
-	movq	%rax, %rdi
-	movl	$0, %eax
-	call	printf@PLT
-	addl	$1, -16(%rbp)
-.L6:
-	cmpl	$3, -16(%rbp)
-	jle	.L7
-	movl	$10, %edi
-	call	putchar@PLT
-	leaq	.LC6(%rip), %rax
-	movq	%rax, %rdi
-	movl	$0, %eax
-	call	printf@PLT
-	leaq	-28(%rbp), %rax
-	movq	%rax, %rsi
-	leaq	.LC7(%rip), %rax
-	movq	%rax, %rdi
-	movl	$0, %eax
 	call	__isoc99_scanf@PLT
-	movl	$0, -20(%rbp)
-	jmp	.L8
-.L11:
-	movl	-20(%rbp), %eax
+	movl	$0, -28(%rbp)
+	jmp	.L12
+.L15:
+	movl	-28(%rbp), %eax
 	cltq
-	movzbl	-28(%rbp,%rax), %eax
+	movzbl	-36(%rbp,%rax), %eax
 	movsbl	%al, %eax
 	leal	-48(%rax), %edx
-	movl	-20(%rbp), %eax
+	movl	-28(%rbp), %eax
 	cltq
-	movl	%edx, -48(%rbp,%rax,4)
-	movl	-20(%rbp), %eax
+	movl	%edx, -64(%rbp,%rax,4)
+	movl	-28(%rbp), %eax
 	cltq
-	movl	-48(%rbp,%rax,4), %edx
-	movl	-20(%rbp), %eax
+	movl	-64(%rbp,%rax,4), %edx
+	movl	-28(%rbp), %eax
 	cltq
 	leaq	0(,%rax,4), %rcx
 	leaq	masterCode.0(%rip), %rax
 	movl	(%rcx,%rax), %eax
 	cmpl	%eax, %edx
-	jne	.L9
-	leaq	.LC8(%rip), %rax
+	jne	.L13
+	leaq	.LC4(%rip), %rax
 	movq	%rax, %rdi
 	call	puts@PLT
 	addl	$1, -4(%rbp)
-	jmp	.L10
-.L9:
-	leaq	.LC9(%rip), %rax
+	jmp	.L14
+.L13:
+	leaq	.LC5(%rip), %rax
 	movq	%rax, %rdi
 	call	puts@PLT
 	addl	$1, -8(%rbp)
-.L10:
-	addl	$1, -20(%rbp)
-.L8:
-	cmpl	$3, -20(%rbp)
-	jle	.L11
+.L14:
+	addl	$1, -28(%rbp)
+.L12:
+	cmpl	$3, -28(%rbp)
+	jle	.L15
 	movl	-4(%rbp), %eax
 	movl	%eax, %esi
-	leaq	.LC10(%rip), %rax
+	leaq	.LC6(%rip), %rax
 	movq	%rax, %rdi
 	movl	$0, %eax
 	call	printf@PLT
 	movl	-8(%rbp), %eax
 	movl	%eax, %esi
-	leaq	.LC11(%rip), %rax
+	leaq	.LC7(%rip), %rax
 	movq	%rax, %rdi
 	movl	$0, %eax
 	call	printf@PLT
